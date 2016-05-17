8ab7800cec50e3aaf2b19308be3d212f16076547
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-05-17 13:00:32 -0700

    statefull component Article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,17 +1,32 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 
-function Article(props) {
-    const { article } = props
-    if (!article) return <h3>No article</h3>
+class Article extends Component {
+    state = {
+        isOpen: false
+    }
 
-    const { title, text, id } = article
-    return (
-        <div>
-            <h3>{title}</h3>
-            <section>{text}</section>
-        </div>
-    )
+    render() {
+        const { article } = this.props
+        if (!article) return <h3>No article</h3>
+
+        const { title, text, id } = article
+        const { isOpen } = this.state
+        const textItem = isOpen ? <section>{text}</section> : null
+        return (
+            <div>
+                <h3 onClick = {this.handleClick}>{title}</h3>
+                {textItem}
+            </div>
+        )
+    }
+
+    handleClick = (ev) => {
+        this.setState({
+            isOpen : !this.state.isOpen
+        })
+    }
 }
+
 
 Article.propTypes = {
     article: PropTypes.shape({
