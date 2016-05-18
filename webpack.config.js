3e5dba7064043c2814d81af495b0755f75c76d9c
Гранев Кирилл Евгеньевич <granev@waptak.ru>
2016-05-18 18:23:53 +0300

    add json in the app

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ const scriptsPath = path.resolve(__dirname, 'data');
 
 module.exports = {
     devtool: 'eval',
-    entry: {
-        // main: path.join(scriptsPath, 'index.js'),
-        app: path.join(__dirname, '/src/app.js')
-    },
+    entry: [
+        './src/app.js'
+    ],
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'bundle.js',
@@ -20,8 +19,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            {   test: /\.json?$/,
-                loader: 'json'
+            {   test: /\.json$/,
+                loader: 'raw-loader'
             }, {
                 test: /\.jsx?/,
                 loaders: ['babel'],
