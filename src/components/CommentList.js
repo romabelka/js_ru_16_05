bb9ca14e6813f623aa74aa768e4a50bad3c46d9c
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-05-24 12:48:24 -0700

    use toggleOpen in commentList

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,13 +1,10 @@
 import React, { Component, PropTypes } from 'react'
 import Comment from './Comment'
+import toggleOpen from '../decorators/toggleOpen'
 
 class CommentList extends Component {
     static propTypes = {
         comments: PropTypes.array
-    };
-
-    state = {
-        isOpen: false
     };
 
     render() {
@@ -20,24 +17,18 @@ class CommentList extends Component {
     }
 
     getToggler() {
-        const text = this.state.isOpen ? 'hide comments' : 'show comments'
-        return <a href = "#" onClick = {this.toggleOpen}>{text}</a>
-    }
-
-    toggleOpen = (ev) => {
-        ev.preventDefault()
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
+        const { isOpen, toggleOpen } = this.props
+        const text = isOpen ? 'hide comments' : 'show comments'
+        return <a href = "#" onClick = {toggleOpen}>{text}</a>
     }
 
     getList() {
-        if (!this.state.isOpen) return null
-        const { comments } = this.props
+        const { isOpen, comments } = this.props
+        if (!isOpen) return null
         if (!comments || !comments.length) return <h3>No comments yet</h3>
         const items = comments.map(comment => <li key = {comment.id}><Comment comment = {comment} /></li>)
         return <ul>{items}</ul>
     }
 }
 
-export default CommentList
+export default toggleOpen(CommentList)
