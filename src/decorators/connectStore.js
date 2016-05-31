f76104b5e979f4eb527225960f1244b44f98916b
Roman <kaboom.ua@gmail.com>
2016-05-30 20:59:58 -0700

    Update connectStore.js

--- src/decorators/connectStore.js
+++ src/decorators/connectStore.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { stores } from '../stores/';
 
 export default (CustomComponent) => {
-
+	//1) в декораторе надо подписываться на изменения в сторах
+	//2) передавать дочернему компоненту надо не сам стор, а кокой-то набор данных, который должен храниться в state
 	return class DecoratorComponent extends Component {
 		constructor() {
 	        super();
