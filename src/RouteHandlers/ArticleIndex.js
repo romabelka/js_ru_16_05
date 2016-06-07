ad8fe8b65900f0ba448c5afd6694fe4bda0e9d4e
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-06-07 11:58:52 -0700

    add index Route and redirect

---
+++ src/RouteHandlers/ArticleIndex.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+
+function ArticleIndex(props) {
+    return (
+        <div>
+            <h3>Choose your article</h3>
+        </div>
+    )
+}
+
+ArticleIndex.propTypes = {
+}
+
+export default ArticleIndex
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,20 @@
-import { Router, Route, hashHistory, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, IndexRedirect, hashHistory, browserHistory } from 'react-router'
 import React from 'react'
-import ArticleIndex from './RouteHandlers/ArticlesIndex'
+import ArticleRoot from './RouteHandlers/ArticlesRoot'
+import ArticleIndex from './RouteHandlers/ArticleIndex'
 import ArticlePage from './RouteHandlers/ArticlePage'
 import CommentsRoot from './RouteHandlers/CommentsRoot'
 import CommentsPage from './RouteHandlers/CommentsPage'
 
 export default (
     <Router history = {browserHistory}>
-        <Route path = "/articles" component = {ArticleIndex}>
+        <Redirect from = "/" to = "/articles"/>
+        <Route path = "/articles" component = {ArticleRoot}>
+            <IndexRoute component = {ArticleIndex}/>
             <Route path = ":id" component = {ArticlePage} />
         </Route>
-        <Route path = "comments" component = {CommentsRoot} >
+        <Route path = "/comments" component = {CommentsRoot} >
+            <IndexRedirect to = "1"/>
             <Route path = ":page" component = {CommentsPage} />
         </Route>
     </Router>
