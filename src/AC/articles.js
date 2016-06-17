57d81044293abffd2883feaa2c5b7bf6d451136c
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-06-17 12:18:35 -0700

    add redux-multi

--- src/AC/articles.js
+++ src/AC/articles.js
@@ -1,8 +1,31 @@
-import { DELETE_ARTICLE } from '../constants'
+import { DELETE_ARTICLE, LOAD_ALL_ARTICLES, START, SUCCESS, FAIL } from '../constants'
+import $ from 'jquery'
 
 export function deleteArticle(id) {
     return {
         type: DELETE_ARTICLE,
         payload: { id }
+    }
+}
+
+/*
+//For Hometask
+
+export function loadAllArticles() {
+    return {
+        type: LOAD_ALL_ARTICLES,
+        callAPI: '/article'
+    }
+}*/
+
+export function loadAllArticles() {
+    return (dispatch, getState) => {
+        dispatch({
+            type: LOAD_ALL_ARTICLES + START
+        })
+
+        $.get('/article')
+            .done(response => dispatch({ type: LOAD_ALL_ARTICLES + SUCCESS, response}))
+            .fail(error => dispatch({type: LOAD_ALL_ARTICLES + FAIL, error}))
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from '../reducer'
 import randomId from '../middlewares/randomId'
 import logger from '../middlewares/logger'
+import thunk from 'redux-thunk'
 
 const enhancer = compose(
-    applyMiddleware(randomId, logger),
+    applyMiddleware(randomId, logger, thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
 )
 
